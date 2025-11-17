093a81c3f71d574bb1e2c6c6cab666e415509693
Ace <adamabdallahayln1@gmail.com>
2025-11-17 21:28:42 +0200

    deploying project

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/mugs-coffee-website/',
   plugins: [
     react(),
     tailwindcss(),
+    
   ],
 })
