1a51feb7d064a6f9c0b082a116fd2094d37ff90c
Ace <adamabdallahayln1@gmail.com>
2025-12-11 16:55:46 +0200

    Initailizing images

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -4,35 +4,46 @@ import { IoTime } from "react-icons/io5";
 
 const Contact = () => {
   return (
-    <section id='contact' className='bg-white mt-10 md:mt-20 flex flex-col p-5 justify-center items-center gap-10'>
-        <h1 className='text-3xl md:text-4xl font-extrabold text-center'>Contact Us</h1>
+    <section
+      id="contact"
+      className="bg-white py-16 px-6 flex flex-col items-center"
+    >
+      <h1 className="text-4xl md:text-5xl font-extrabold text-center mb-10">
+        Contact Us
+      </h1>
 
-        <div className='bg-black w-full text-white flex flex-col md:flex-row justify-around items-center gap-6 p-6 md:p-10 rounded-lg'>
-
-            <div className='flex flex-col items-center gap-2 text-center'>
-                <FaMapMarkedAlt className='text-4xl md:text-5xl text-[#C69B7B]'/>
-                <h1 className='text-xl md:text-2xl font-bold'>Address</h1>
-                <p className='text-sm md:text-base'>Deir El Zahrani | Drive Thru</p>
-                <p className='text-sm md:text-base'>Nabatieh Kfarjoz | Drive Thru</p>
-            </div>
-
-            <div className='flex flex-col items-center gap-2 text-center'>
-                <FaPhoneAlt className='text-4xl md:text-5xl text-[#C69B7B]'/>
-                <h1 className='text-xl md:text-2xl font-bold'>Call Us</h1>
-                <p className='text-sm md:text-base'>Kfarjoz: +961-76-444-617</p>
-                <p className='text-sm md:text-base'>Der El Zahrani: +961-71-444-836</p>
-            </div>
-
-            <div className='flex flex-col items-center gap-2 text-center'>
-                <IoTime className='text-4xl md:text-5xl text-[#C69B7B]'/>
-                <h1 className='text-xl md:text-2xl font-bold'>Opening Hours</h1>
-                <p className='text-sm md:text-base'>Mon - Fri: 7 AM - 7 PM</p>
-                <p className='text-sm md:text-base'>Sat - Sun: 8 AM - 6 PM</p>
-            </div>
-
+      <div
+        className="
+          bg-black/90 w-full max-w-6xl 
+          text-white rounded-2xl 
+          grid grid-cols-1 md:grid-cols-3 
+          gap-10 p-10
+        "
+      >
+        <div className="flex flex-col items-center gap-3 text-center">
+          <FaMapMarkedAlt className="text-5xl text-[#C69B7B]" />
+          <h2 className="text-2xl font-bold">Address</h2>
+          <p className="text-sm md:text-base opacity-80">Deir El Zahrani | Drive Thru</p>
+          <p className="text-sm md:text-base opacity-80">Nabatieh Kfarjoz | Drive Thru</p>
         </div>
-    </section>
-  )
-}
 
-export default Contact
+        {/* Call Us */}
+        <div className="flex flex-col items-center gap-3 text-center">
+          <FaPhoneAlt className="text-5xl text-[#C69B7B]" />
+          <h2 className="text-2xl font-bold">Call Us</h2>
+          <p className="text-sm md:text-base opacity-80">Kfarjoz: +961-76-444-617</p>
+          <p className="text-sm md:text-base opacity-80">Deir El Zahrani: +961-71-444-836</p>
+        </div>
+
+        <div className="flex flex-col items-center gap-3 text-center">
+          <IoTime className="text-5xl text-[#C69B7B]" />
+          <h2 className="text-2xl font-bold">Opening Hours</h2>
+          <p className="text-sm md:text-base opacity-80">Mon - Fri: 7 AM - 7 PM</p>
+          <p className="text-sm md:text-base opacity-80">Sat - Sun: 8 AM - 6 PM</p>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Contact;
--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -1,106 +1,255 @@
+import Affogato from '../assets/Affogato.jpg'
+import Americano from '../assets/Americano.jpg'
+import BelgianChoclate from '../assets/BelgianChoclate.jpg'
+import BulkPro from '../assets/BulkPro.jpg'
+import Cappussiono from '../assets/Cappussiono.jpg'
+import CaramelFrappe from '../assets/CaramelFrappe.jpg'
+import CaramelMacchiato from '../assets/CaramelMacchiato.jpg'
+import CaramelMilkshake from '../assets/CaramelMilkshake.jpg'
+import ChickenCaesarSalad from '../assets/ChickenCaesarSalad.jpg'
+import ChoclateMilkshake from '../assets/ChoclateMilkshake.jpg'
+import ChoclatePro from '../assets/ChoclatePro.jpg'
+import CinnamonRoll from '../assets/CinnamonRoll.jpg'
+import coffeeImage from '../assets/mugs-logo.svg'
+import Cortado from '../assets/Cortado.jpg'
+import CrabNoodlesSalad from '../assets/CrabNoodlesSalad.jpg'
+import CreamySalmon from '../assets/CreamySalmon.jpg'
+import CriossantAmande from '../assets/criossantAmande.jpg'
+import CriossantBeurre from '../assets/CriossantBeurre.jpg'
+import criossantBeurreThym from '../assets/criossantBeurreThym.jpg'
+import criossantFromage from '../assets/criossantFromage.jpg'
+import DarkMochaFrappe from '../assets/DarkMochaFrappe.jpg'
+import DarkMocha from '../assets/DarkMocha.jpg'
+import doubleespresso from '../assets/doubleespresso.jpg'
+import DoubleMochaE from '../assets/DoubleMochaE.jpg'
+import Espresso from '../assets/Espresso.jpg'
+import flatwhite from '../assets/flatwhite.jpg'
+import FlavoredLatte from '../assets/FlavoredLatte.jpg'
+import FlexPro from '../assets/FlexPro.jpg'
+import FuelPro from '../assets/FuelPro.jpg'
+import GrapeMint from '../assets/GrapeMint.jpg'
+import HalloumiPesto from '../assets/HalloumiPesto.jpg'
+import HotDoubleShot from '../assets/HotDoubleShot.jpg'
+import HotVanillaMathca from '../assets/HotVanillaMathca.jpg'
+import IcedAmericano from '../assets/IcedAmericano.jpg'
+import IcedCappuccino from '../assets/IcedCappuccino.jpg'
+import IcedCaramelMacchiato from '../assets/IcedCaramelMacchiato.jpg'
+import IcedDarkMocha from '../assets/IcedDarkMocha.jpg'
+import IcedFlavoredLatte from '../assets/IcedFlavoredLatte.jpg'
+import IcedFrenchVanilla from '../assets/IcedFrenchVanilla.jpg'
+import IcedLatte from '../assets/IcedLatte.jpg'
+import IcedPassionSmoothie from '../assets/IcedPassionSmoothie.jpg'
+import IcedSaltedCaramel from '../assets/IcedSaltedCaramel.jpg'
+import IcedSpanish from '../assets/IcedSpanish.jpg'
+import IcedWhiteMocha from '../assets/IcedWhiteMocha.jpg'
+import Latte from '../assets/Latte.jpg'
+import LemonMint from '../assets/LemonMint.jpg'
+import LotusMilkshake from '../assets/LotusMilkshake.jpg'
+import MentedLemon from '../assets/MentedLemon.jpg'
+import MangoIcedMatcha from '../assets/MangoIcedMatcha.jpg'
+import MangoPassionSmoothie from '../assets/MangoPassionSmoothie.jpg'
+import MangoSmoothie from '../assets/MangoSmoothie.jpg'
+import MatchaFrappe from '../assets/MatchaFrappe.jpg'
+import MixedSmoothie from '../assets/MixedSmoothie.jpg'
+import mugsLogoBlack from '../assets/mugs-logo-black.svg'
+import mugsLogo from '../assets/mugs-logo.svg'
+import NakhleApple from '../assets/NakhleApple.jpg'
+import PainAuChocolat from '../assets/PainAuChocolat.jpg'
+import PainAuxRaisins from '../assets/PainAuxRaisins.jpg'
+import PinkLemonSmoothie from '../assets/PinkLemonSMoothie.jpg'
+import PistachioMilkshake from '../assets/PistachioMilkshake.jpg'
+import QuinaoSalad from '../assets/QuinaoSalad.jpg'
+import SaltedCaramelFrappe from '../assets/SaltedCaramelFrappe.jpg'
+import SaltedCaramel from '../assets/SaltedCaramel.jpg'
+import ShakenWhiteMocha from '../assets/ShakenWhiteMocha.jpg'
+import SpanishLatte from '../assets/SpanishLatte.jpg'
+import StrawberyIcedMatcha from '../assets/StrawberyIcedMatcha.jpg'
+import StrawberyMilkshake from '../assets/StrawberyMilkshake.jpg'
+import StrawberySmoothie from '../assets/StrawberySmoothie.jpg'
+import SuisseLongueRaisin from '../assets/SuisseLongueRaisin.jpg'
+import TunaSalad from '../assets/TunaSalad.jpg'
+import TurkeyandCheese from '../assets/TurkeyandCheese.jpg'
+import Turkish from '../assets/Turkish.jpg'
+import VanillaFrappe from '../assets/VanillaFrappe.jpg'
+import VanillaIcedMatcha from '../assets/VanillaIcedMatcha.jpg'
+import VanillaMilkshake from '../assets/VanillaMilkshake.jpg'
+import WhiteMochaFrapper from '../assets/WhiteMochaFrapper.jpg'
+import WhiteMocha from '../assets/WhiteMocha.jpg'
+import LabnehBaladeyeh from '../assets/LabnehBaladeyeh.jpg'
 
 const menuData = [
   {
     category: "Hot Drinks",
     items: [
+      { image: Espresso, name: "Espresso", price: "$2.00" },
+      { image: doubleespresso, name: "Double Espresso", price: "$3.00" },
+      { image: Cortado, name: "Cortado", price: "$4.00" },
+      { image: flatwhite, name: "Flat White", price: "$4.00" },
+      { image: DarkMocha, name: "Dark Mocha", price: "$5.00" },
+      { image: WhiteMocha, name: "White Mocha", price: "$5.00" },
+      { image: Cappussiono, name: "Cappuccino", price: "$4.00" },
       {
-        name: "Espresso",
-        price: "$2.0",
+        image: FlavoredLatte,
+        name: "Flavoured Latte (Caramel - Hazelnut - Vanilla)",
+        price: "$4.50"
       },
-      {
-        name: "Double Expresso",
-        price: "$3.0",
-      },
-      {
-        name: "Cappuccino",
-        price: "$4.0",
-      },
-      {
-        name: "Americano",
-        price: "$4.0",
-      }
+      { image: Americano, name: "Americano", price: "$4.00" },
+      { image: Latte, name: "Latte", price: "$4.00" },
+      { image: DoubleMochaE, name: "Double Mocha Macchiato", price: "$5.00" },
+      { image: HotDoubleShot, name: "Hot Double Shot", price: "$5.00" },
+      { image: IcedFrenchVanilla, name: "French Vanilla", price: "$5.00" },
+      { image: CaramelMacchiato, name: "Caramel Macchiato", price: "$5.00" },
+      { image: SaltedCaramel, name: "Salted Caramel", price: "$5.00" },
+      { image: SpanishLatte, name: "Spanish Latte", price: "$4.50" },
+      { image: Affogato, name: "Affogato (+ Vanilla Ice Cream)", price: "$5.00" },
+      { image: Turkish, name: "Turkish", price: "$3.00" },
+      { image: BelgianChoclate, name: "Belgian Chocolate", price: "$6.00" }
     ]
   },
-  {
-    category: "Milkshakes",
-    items: [
-      {
-        name: "Chocltate",
-        price: "$5.0",
-      },
-            {
-        name: "Strawberry",
-        price: "$5.0",
-      },
-            {
-        name: "Vanilla",
-        price: "$5.0",
-      }
-      
-      
-      
-    ]
-  },
-  {
-    category: "Smoothies",
-    items: [
-      {
-        name: "Mango",
-        price: "$6.0",
-      },
-      {
-        name: "Strawberry",
-        price: "$6.0",
-      }
-    ]
-  },
+
   {
     category: "Iced Coffee",
     items: [
+      { image: IcedLatte, name: "Iced Latte", price: "$4.00" },
       {
-        name: "Iced Latte",
-        price: "$4.0"
+        image: IcedFlavoredLatte,
+        name: "Flavoured Latte (Iced)",
+        price: "$5.00"
       },
-      {
-        name: "Flavored Latte",
-        price: "$5.0"
-      },
-      {
-        name: "Americano",
-        price: "$4.0"
-      },
-      {
-        name: "Spanish",
-        price: "$5.0"
-      },
-      {
-        name: "Fresh Vanilla",
-        price: "$5.0"
-      }
+      { image: IcedAmericano, name: "Americano (Iced)", price: "$4.00" },
+      { image: IcedSpanish, name: "Spanish (Iced)", price: "$5.00" },
+      { image: IcedFrenchVanilla, name: "French Vanilla (Iced)", price: "$5.00" },
+      { image: ShakenWhiteMocha, name: "Shaken White Mocha", price: "$5.00" },
+      { image: IcedDarkMocha, name: "Dark Mocha (Iced)", price: "$5.00" },
+      { image: IcedSaltedCaramel, name: "Salted Caramel (Iced)", price: "$5.00" },
+      { image: IcedCaramelMacchiato, name: "Caramel Macchiato (Iced)", price: "$5.00" },
+      { image: IcedCappuccino, name: "Cappuccino (Iced)", price: "$4.50" },
+      { image: IcedWhiteMocha, name: "White Mocha (Iced)", price: "$5.00" }
     ]
   },
+
+  {
+    category: "Milkshakes",
+    items: [
+      { image: ChoclateMilkshake, name: "Chocolate Milkshake", price: "$5.00" },
+      { image: StrawberyMilkshake, name: "Strawberry Milkshake", price: "$5.00" },
+      { image: VanillaMilkshake, name: "Vanilla Milkshake", price: "$5.00" },
+      { image: LotusMilkshake, name: "Lotus Milkshake", price: "$6.00" },
+      {
+        image: CaramelMilkshake,
+        name: "Caramel Pretzels Milkshake",
+        price: "$5.50"
+      },
+      { image: PistachioMilkshake, name: "Pistachio Milkshake", price: "$6.00" }
+    ]
+  },
+
+  {
+    category: "Smoothies",
+    items: [
+      { image: MangoSmoothie, name: "Mango Smoothie", price: "$6.00" },
+      { image: StrawberySmoothie, name: "Strawberry Smoothie", price: "$6.00" },
+      { image: MixedSmoothie, name: "Mixed Berries Smoothie", price: "$6.50" },
+      { image: MentedLemon, name: "Minted Lemonade", price: "$5.00" },
+      { image: PinkLemonSmoothie, name: "Pink Lemonade Smoothie", price: "$5.00" },
+      { image: IcedPassionSmoothie, name: "Iced Passion Smoothie", price: "$6.00" },
+      { image: MangoPassionSmoothie, name: "Mango Passion Smoothie", price: "$6.00" }
+    ]
+  },
+
   {
     category: "Frappe",
     items: [
-      {
-        name: "Caramel Frappe",
-        price: "$5.0"
-      },
-      {
-        name: "White Mocha Frappe",
-        price: "$5.0"
-      },
-      {
-        name: "Dark Mocha Frapper",
-        price: "$5.0"
-      },
-      {
-        name: "Vanilla Frappe",
-        price: "$5.0"
-      }
+      { image: CaramelFrappe, name: "Caramel Frappe", price: "$5.00" },
+      { image: WhiteMochaFrapper, name: "White Mocha Frappe", price: "$5.00" },
+      { image: DarkMochaFrappe, name: "Dark Mocha Frappe", price: "$5.00" },
+      { image: SaltedCaramelFrappe, name: "Salted Caramel Frappe", price: "$5.00" },
+      { image: VanillaFrappe, name: "Vanilla Frappe", price: "$5.00" }
+    ]
+  },
+
+  {
+    category: "Matcha Collection",
+    items: [
+      { image: HotVanillaMathca, name: "Hot Vanilla Matcha", price: "$5.00" },
+      { image: VanillaIcedMatcha, name: "Vanilla Iced Matcha", price: "$5.00" },
+      { image: MangoIcedMatcha, name: "Mango Iced Matcha", price: "$6.00" },
+      { image: StrawberyIcedMatcha, name: "Strawberry Iced Matcha", price: "$6.00" },
+      { image: MatchaFrappe, name: "Strawberry Matcha Frappe", price: "$6.00" },
+      { image: MatchaFrappe, name: "Matcha Frappe", price: "$6.00" }
+    ]
+  },
+
+  {
+    category: "Protein Shakes",
+    items: [
+      { image: FlexPro, name: "Flex Pro (Peanut Butter, Banana)", price: "$7.00" },
+      { image: FuelPro, name: "Fuel Pro (Strawberry, Pineapple)", price: "$7.00" },
+      { image: ChoclatePro, name: "Chocolate Pro (Pure Belgian Chocolate)", price: "$7.00" },
+      { image: BulkPro, name: "Bulk Pro (Mango, Pineapple)", price: "$7.00" }
+    ]
+  },
+
+  {
+    category: "Sandwiches",
+    items: [
+      { image: HalloumiPesto, name: "Halloumi Pesto", price: "$6.00" },
+      { image: TurkeyandCheese, name: "Turkey and Cheese", price: "$6.50" },
+      { image: CreamySalmon, name: "Creamy Salmon", price: "$9.00" },
+      { image: LabnehBaladeyeh, name: "Labneh Baladeyeh", price: "$5.00" }
+    ]
+  },
+
+  {
+    category: "Salads",
+    items: [
+      { image: QuinaoSalad, name: "Quinoa Salad", price: "$6.50" },
+      { image: ChickenCaesarSalad, name: "Chicken Caesar Salad", price: "$7.50" },
+      { image: TunaSalad, name: "Tuna Salad", price: "$8.50" },
+      { image: CrabNoodlesSalad, name: "Crab Noodles Salad", price: "$6.50" },
+      { image: QuinaoSalad, name: "Mexican Salad", price: "$6.50" },
+      { image: QuinaoSalad, name: "Kale Salad", price: "$6.50" }
+    ]
+  },
+
+  {
+    category: "Croissants",
+    items: [
+      { image: CriossantBeurre, name: "Croissant Beurre", price: "$3.00" },
+      { image: criossantBeurreThym, name: "Croissant Beurre Thym", price: "$3.00" },
+      { image: criossantFromage, name: "Croissant Beurre Fromage", price: "$3.50" },
+      { image: CriossantAmande, name: "Croissant Beurre Amande", price: "$3.50" },
+      { image: PainAuChocolat, name: "Pain Au Chocolat", price: "$3.50" },
+      { image: CinnamonRoll, name: "Cinnamon Roll", price: "$5.00" },
+      { image: PainAuxRaisins, name: "Pain Aux Raisins", price: "$5.00" },
+      { image: SuisseLongueRaisin, name: "Suisse Longue Raisin", price: "$5.00" },
+      { image: coffeeImage, name: "Spinach Fetta Pretzel", price: "$4.50" }
+    ]
+  },
+
+  {
+    category: "Desserts",
+    items: [
+      { image: coffeeImage, name: "Chocolate Cookie", price: "$3.50" },
+      { image: coffeeImage, name: "Vanilla Cookie", price: "$3.50" },
+      { image: coffeeImage, name: "Brownies", price: "$5.00" },
+      { image: coffeeImage, name: "Light Free Oat Scone", price: "$3.50" },
+      { image: coffeeImage, name: "Lazy Cake", price: "$5.00" },
+      { image: coffeeImage, name: "Fudge Cake", price: "$4.50" },
+      { image: coffeeImage, name: "Lotus Cheese Cake", price: "$4.50" },
+      { image: coffeeImage, name: "Tarte", price: "$4.50" },
+      { image: coffeeImage, name: "Chocolate Muffin", price: "$3.50" },
+      { image: coffeeImage, name: "Banana Muffin", price: "$4.00" },
+      { image: coffeeImage, name: "Vanilla Muffin", price: "$3.50" },
+      { image: coffeeImage, name: "Tiramisu", price: "$5.00" },
+      { image: coffeeImage, name: "High Protein Brownies", price: "$6.00" },
+      { image: coffeeImage, name: "High Protein Chocolate", price: "$6.00" },
+      { image: coffeeImage, name: "Carrot Cake", price: "$4.50" },
+      { image: coffeeImage, name: "Oreo Cheese Cake", price: "$4.50" },
+      { image: coffeeImage, name: "Honey Cake", price: "$4.50" }
     ]
   }
-];
+]
 
 import React, { useState } from 'react';
 
@@ -133,7 +282,10 @@ const Menu = () => {
           {category.items.map(item => (
             <div key={item.name} className='border-b pb-4'>
               <div className='flex justify-between items-center'>
-                <h2 className='text-2xl font-semibold'>{item.name}</h2>
+                <div className='flex justify-center items-center'>
+                  <img className='w-30' src={item.image} alt="" />
+                  <h2 className='text-2xl font-semibold'>{item.name}</h2>
+                </div>
                 <span className='font-bold'>{item.price}</span>
               </div>
               <p className='text-gray-700 mt-1'>{item.description}</p>
